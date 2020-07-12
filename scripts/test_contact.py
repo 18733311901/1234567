3e6f1312ccf9c412b88aeff19d8243c48bfd5eb9
18733311901 <392647620@qq.com>
2020-07-12 15:07:50 +0800

    '123'

--- scripts/test_contact.py
+++ scripts/test_contact.py
@@ -10,9 +10,9 @@ class TestContact():
     def test_hello4(self):
         assert 1
     def test_hello5(self):
-        assert 0
+        assert 1
     def test_hello6(self):
-        assert 0
+        assert 1
     def test_hello7(self):
         assert 1
     def test_hello8(self):
